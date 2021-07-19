d19a44f15cc969b7462e73868414a92e17d65921
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-07-19 19:53:43 +0900

    각 화면에 대한 클라이언트 파일

---
+++ server/public/login.js
@@ -0,0 +1,25 @@
+const loginForm = document.querySelector("#login-form");
+const loginButton = document.querySelector("#login-button");
+const inputUserId = document.querySelector("#user-id");
+const inputUserPwd = document.querySelector("#pwd");
+
+loginForm.addEventListener("submit",async (evt)=>{
+    evt.preventDefault();
+    if(inputUserId.value==="" || inputUserPwd.value===""){
+        alert("모두다 입력해주세요");
+    }else{
+        const isLogin = await axios.post("/login",{
+            userId:inputUserId.value,
+            pwd:inputUserPwd.value
+        });
+        if(isLogin.data.success){
+            alert("로그인 성공")
+            history.pushState(null,null,"/chat");
+            location.reload();
+        }
+        else{
+            alert("아이디나 비밀번호를 다시 확인해보세요")
+        }
+
+    }
+})
---
+++ server/public/register.js
@@ -0,0 +1,72 @@
+const registerForm = document.querySelector("#register-form");
+const checkUniqueName = document.querySelector("#check-name-unique");
+const inputUserId = document.querySelector("#user-id");
+const inputId = document.querySelector("#id");
+const inputUserPwd = document.querySelector("#pwd");
+const inputUserPwdPwd = document.querySelector("#pwd-pwd");
+const radioMale = document.querySelector("#male");
+const radioFemale = document.querySelector("#female");
+const inputAge = document.querySelector("#age");
+
+let nameUnique = false;
+// 닉네임 중복체크
+checkUniqueName.addEventListener("click",async ()=>{
+    if(inputUserId.value===""){
+        alert("닉네임을 입력해주세요")
+    }else{
+        console.log(inputUserId.value)
+        const result = await axios.post("/register",{
+            checkUnique:true,
+            nickName:inputUserId.value
+        });
+        if (result.data.isUnique){
+            alert("사용할수 있는 이름입니다.");
+            nameUnique=true;
+        }else{
+            alert("이미 존재하는 이름입니다.");
+            nameUnique = false;
+        }
+    }
+
+})
+
+// 회원가입
+registerForm.addEventListener("submit",async (evt)=>{
+    evt.preventDefault();
+    // 중복체크를 안했을때
+    if (!nameUnique){
+        alert("중복체크를 해주세요");
+        return;
+    }
+    // 비번이 안맞을때
+    if(!(inputUserPwd.value===inputUserPwdPwd.value)){
+        alert("비밀번호가 다릅니다.");
+        return;
+
+    }
+    // 빈칸있을때
+    if(inputUserId.value==="" || inputId.value==="" || inputUserPwd.value==="" || inputUserPwdPwd.value==="" || (!radioFemale.checked &&
+    !radioMale.checked) || inputAge.value===""){
+        alert("모두 입력해주세요");
+    }
+    else{
+        await axios.post("/register",{
+            checkUnique:undefined,
+            userId:inputId.value,
+            userPwd:inputUserPwd.value,
+            userName:inputUserId.value,
+            age:inputAge.value,
+            gender:radioMale.checked
+        });
+        inputId.value = "";
+        inputUserId.value="";
+        inputUserPwdPwd.value="";
+        inputUserPwd.value="";
+        inputAge.value="";
+        radioMale.checked=false;
+        radioFemale.checked=false;
+    }
+
+
+});
+
